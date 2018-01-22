0f32de3495e737a8fbbcf1020dbaae39371fd21e
GimpG1 <karool.dwojak@gmail.com>
2018-01-22 22:53:57 +0100

    Gui & PlaceHolder

--- Assets/Scripts/Controllers/GUI/InGameGUIController.cs
+++ Assets/Scripts/Controllers/GUI/InGameGUIController.cs
@@ -1,11 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class InGameGUIController : MonoBehaviour {
+public class InGameGUIController : MonoBehaviour
+{
+
+    #region Private Variables
+    private Button _backToGameBtn;
+    private Button _settingsBtn;
+    private Button _exitBtn;
+    private Button _backBtn;
+
+    private Slider _soundSR;
+    private Slider _sfxSR;
+
+    private Text _soundInfoTxt;
+    private Text _sfxInfoTxt;
+
+    private Canvas _inCanvas;
+    [SerializeField] GameObject _menuCanvas;
+
+    #endregion
+
+    private void Awake()
+    {
+        _backToGameBtn = GameObject.Find("InBackToGameBTN").GetComponent<Button>();
+        _settingsBtn = GameObject.Find("InSettingsBTN").GetComponent<Button>();
+        _exitBtn = GameObject.Find("InExitGameBTN").GetComponent<Button>();
+        _backBtn = GameObject.Find("InBackBTN").GetComponent<Button>();
+
+        _soundSR = GameObject.Find("InSoundVolumeSR").GetComponent<Slider>();
+        _sfxSR = GameObject.Find("InSFXVolumeSR").GetComponent<Slider>();
+
+        _soundInfoTxt = GameObject.Find("InSoundInfo").GetComponent<Text>();
+        _sfxInfoTxt = GameObject.Find("InSFXInfo").GetComponent<Text>();
+
+        _inCanvas = gameObject.GetComponent<Canvas>();
+        _inCanvas.enabled = false;
+    }
 
     public void OnEscape()
     {
+        
         Debug.Log("Esc pressed");
     }
 }
